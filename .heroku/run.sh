e87eb19f5b0eebc0705b7805038bd467f52198f1
pius.lee <pius.lee@samsung.com>
2021-09-02 14:15:50 +0900

    fix build script

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -1,3 +1,2 @@
-cd pyvtracer
-maturin develop
-cd -
+maturin build -m pyvtracer/Cargo.toml -i python3.9
+find target -name vlib*p39*.whl -exec pip install {} \;
---
+++ .profile.d/buildpack_env.sh
@@ -0,0 +1,2 @@
+export RUST_SKIP_BUILD=1
+export RUST_INSTALL_DIESEL=1
