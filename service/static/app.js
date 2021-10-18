6c0ddc619f19036fe79f3ad9cc31108578b289a2
pius.lee <pius.lee@samsung.com>
2021-10-18 11:43:31 +0900

    change ws name to fix unintended overwrite

--- service/static/app.js
+++ service/static/app.js
@@ -5,22 +5,22 @@ if (window.location.protocol == "https:") {
     var ws_scheme = "ws://"
 };
 
-var screen = new ReconnectingWebSocket(ws_scheme + location.host + "/register");
+var ws = new ReconnectingWebSocket(ws_scheme + location.host + "/register");
 
-screen.onopen = function() {
-    screen.send('1234');
+ws.onopen = function() {
+    ws.send('1234');
 }
 
 var contentsWrapper = document.querySelector('.contents-wrapper');
 
-screen.onmessage = function(msg) {
+ws.onmessage = function(msg) {
     contentsWrapper.innerHTML = msg.data;
     let svg = contentsWrapper.firstChild;
     let snap = Snap(svg)
     snap.attr({width: '100%', height: '100%', preserveAspectRatio: 'none'});
     document.body.style.backgroundColor = 'black';
 }
-screen.onclose = function() {
+ws.onclose = function() {
     console.log('closed');
-    screen = new WebSocket(screen.url);
+    ws = new WebSocket(ws.url);
 }
